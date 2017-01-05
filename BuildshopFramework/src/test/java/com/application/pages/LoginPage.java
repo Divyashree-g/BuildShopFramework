468c56d2a5c1f3f86d816c64b801fe8dada2dcde
gonchideepak <gonchideepak@gmail.com>
2017-01-05 15:01:21 +0530

    oo

--- BuildshopFramework/src/test/java/com/application/pages/LoginPage.java
+++ BuildshopFramework/src/test/java/com/application/pages/LoginPage.java
@@ -7,11 +7,10 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-	private WebDriver driver;
+ 	
 
 	public LoginPage(WebDriver driver)
 	{
-		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 	
